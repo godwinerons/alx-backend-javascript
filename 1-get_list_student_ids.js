657d567809853c38f468b8c5864d1a3fea11e232
godwinerons <eronsmahon@gmail.com>
2023-07-05 04:26:56 +0100

    completed

--- 1-get_list_student_ids.js
+++
@@ -1,6 +0,0 @@
-export default function getListStudentIds(students) {
-  if (Array.isArray(students)) {
-    return students.map((items) => items.id);
-  }
-  return [];
-}
--- 3-get_ids_sum.js
+++
@@ -1,6 +0,0 @@
-export default function getStudentIdsSum(students) {
-  if (Array.isArray(students)) {
-    return students.reduce((accumulator, each) => accumulator + each.id, 0);
-  }
-  return [];
-}
